b9ace841a1254226f7cea6f60af1d794ed94c991
heikoschmidt1187 <github@heikoschmidt.info>
2020-01-21 18:55:58 +0100

    implemented kdtree search

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -48,12 +48,42 @@ struct KdTree
 		insertHelper(root, 0, point, id);
 	}
 
+	void searchHelper(Node *node, int depth, std::vector<int>& ids, std::vector<float> target, float distanceTol)
+	{
+		if(node == NULL)
+			return;
+
+		// check if node is in the target box
+		if(		(node->point.at(0) >= (target.at(0) - distanceTol))
+			&&	(node->point.at(0) <= (target.at(0) + distanceTol))
+			&&	(node->point.at(1) >= (target.at(1) - distanceTol))
+			&&	(node->point.at(1) <= (target.at(1) + distanceTol))) {
+
+			float x = node->point.at(0) - target.at(0);
+			float y = node->point.at(1) - target.at(1);
+			float dist = sqrt(x*x + y*y);
+
+			// check if need to add
+			if(dist <= distanceTol)
+				ids.push_back(node->id);
+		}
+
+		// check if box is accross boundary
+		if((target.at(depth % 2) - distanceTol) < node->point.at(depth % 2))
+			searchHelper(node->left, depth + 1, ids, target, distanceTol);
+
+		if((target.at(depth % 2) + distanceTol) > node->point.at(depth % 2))
+			searchHelper(node->right, depth + 1, ids, target, distanceTol);
+	}
+
 	// return a list of point ids in the tree that are within distance of target
 	std::vector<int> search(std::vector<float> target, float distanceTol)
 	{
 		std::vector<int> ids;
+
+		// call the search heloper function that traverses recursively
+		searchHelper(root, 0, ids, target, distanceTol);
+
 		return ids;
 	}
-
-
 };
